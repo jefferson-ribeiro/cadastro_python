a71085055867e0564edb229d7661f69e7406a7cd
Jeff <jeffersonari@yahoo.com.br>
2024-10-23 19:30:04 -0300

    salvar dados no arquivo

---
+++ console/ecommerce_app/functions.py
@@ -0,0 +1,191 @@
+import time
+import models as m
+
+# Inicializa as listas para armazenar dados
+clientes = []
+produtos = []
+pedidos = []
+
+def boasvindas():
+    print('╔═════════════════════════════════╗')
+    print('║   Bem-vindo ao E-Shop Brasil    ║')
+    print('║     Tecnologia e Inovação       ║')
+    print('╚═════════════════════════════════╝')
+    time.sleep(2)
+    
+def menu():
+        print('╔═══════════════════════════════════════════╗')
+        print('║               MENU PRINCIPAL              ║')
+        print('╠═══════════════════════════════════════════╣')
+        print('║ 1. Cadastrar Cliente                      ║')
+        print('║ 2. Cadastrar Produto                      ║')
+        print('║ 3. Realizar Pedido                        ║')
+        print('║ 4. Mostrar Clientes                       ║')
+        print('║ 5. Mostrar Produtos                       ║')
+        print('║ 6. Mostrar Pedidos                        ║')
+        print('║ 0. Sair                                   ║')
+        print('╚═══════════════════════════════════════════╝')
+        
+def cadastrar_cliente():
+    while True:
+        # Gera um código de cliente sequencial
+        cod_cliente = len(clientes) + 1
+        print(f'\nCadastro do Cliente {cod_cliente}')
+        # Coleta informações do cliente
+        nome = str(input('Informe o nome completo do cliente: '))
+        cel = str(input(f'Informe o número de celular do cliente [{nome}]: '))
+        cpf = str(input(f'Informe o CPF do cliente [{nome}]: '))
+        email = str(input(f'Informe o email do cliente [{nome}]: '))
+        # Cria instancia Cliente
+        cliente = m.Cliente(cod_cliente, nome, cel, cpf, email)
+        # Adiciona instancia Cliente à lista de clientes
+        clientes.append(cliente)
+         # Salva os clientes após o cadastro
+        salvar_clientes() 
+        # Pergunta se deseja continuar cadastrando mais clientes
+        continuar = str(input('Deseja cadastrar outro cliente? (s/n): ')).lower()
+        if continuar != 's':
+            break
+    
+def cadastrar_produto():
+    while True:
+        # Gera um código de produto sequencial
+        cod_produto = len(produtos) + 1
+        print(f'\nCadastro do produto [{cod_produto}]')
+        # Coleta informações do produto
+        nome_produto = str(input('Informe o nome do produto: '))
+        descricao = str(input(f'Informe uma breve descrição para o produto [{nome_produto}]: '))
+        valor = float(input(f'Informe o valor do produto [{nome_produto}] R$:'))
+        # Cria instancia Produto
+        produto = m.Produto(cod_produto,nome_produto,descricao,valor)
+        # Adiciona o produto à lista de proddutos
+        produtos.append(produto)
+        # Salva os produtos após o cadastro
+        salvar_produtos()  
+        # Pergunta se deseja continuar cadastrando mais produtos
+        continuar = str(input('Deseja cadastrar outro produto? (s/n): ')).lower()
+        if continuar != 's':
+            break
+
+def cadastrar_pedido():
+    while True:
+        # Gera um código de pedido sequencial
+        num_pedido = len(pedidos) + 1
+        print(f'\nCadastro do pedido [{num_pedido}]')
+        # Coleta informações do pedido
+        # Mostra os clientes disponiveis
+        print('══════════════════════ Clientes: ')
+        mostrar_clientes()
+        # Captura código do cliente do pedido
+        cod_cliente_pd = int(input(f'Informe o código cliente para o pedido {num_pedido}: '))
+        # cria variavel para receber o cliente do pedido
+        cliente_selecionado = None
+        # procura o cliente selecionado na lista de clientes
+        for cli in clientes:
+            if cli.codigo == cod_cliente_pd:
+                cliente_selecionado = cli
+                break
+        if cliente_selecionado:
+            print(f'Cliente {cliente_selecionado.nome} selecionado')
+        else:
+            print('Cliente não encontrado.')
+            print('-------------------------------------')
+        
+        # Mostra os produtos disponiveis
+        print('══════════════════════ Produtos: ')
+        mostrar_produtos()
+        # cria lista para armazenar os produtos para o pedido
+        produtos_ped = []
+        while True:
+            # Captura código do produto do pedido
+            cod_produto_pd = int(input(f'Informe o código do produto para o pedido {num_pedido}: '))
+            produto_selecionado = None
+            for prod in produtos:
+                if prod.codigo == cod_produto_pd:
+                    produto_selecionado = prod
+                    break
+            
+            if produto_selecionado:
+                # Captura a quantidade de produto que será adquirida
+                qtd_produto_pd = int(input(f'Informe a quantidade do produto {produto_selecionado.nome} para o pedido {num_pedido}: '))
+                # Adiciona o produto e a quantidade como uma tupla na lista de produtos do pedido
+                produtos_ped.append((produto_selecionado, qtd_produto_pd))
+            else:
+                print('Produto não encontrado.')
+                print('-------------------------------------')
+
+            # Pergunta se deseja continuar cadastrando mais produtos
+            continuar = str(input('Deseja cadastrar outro produto? (s/n): ')).lower()
+            if continuar != 's':
+                break
+
+        # Cria a instância do Pedido
+        pedido = m.Pedido(num_pedido, cliente_selecionado, produtos_ped)
+        # Adiciona o pedido à lista de pedidos
+        pedidos.append(pedido)
+        # Salva os pedidos após o cadastro
+        salvar_pedidos()  
+        print(f'Pedido {num_pedido} cadastrado com sucesso.')
+
+        # Pergunta se deseja cadastrar um novo pedido
+        continuar = str(input('Deseja cadastrar outro pedido? (s/n): ')).lower()
+        if continuar != 's':
+            break
+
+def mostrar_clientes():
+    if not clientes:
+        print('Nenhum cliente cadastrado.')
+        cadastrar_cliente()
+    for cliente in clientes:
+        print(f'COD: {cliente.codigo}')
+        print(f'CLIENTE: {cliente.nome}')
+        print(f'CELULAR: {cliente.telefone}')
+        print(f'CPF: {cliente.cpf}')
+        print(f'EMAIL: {cliente.email}')
+        print('-------------------------------------')
+    
+def mostrar_produtos():
+    if not produtos:
+        print('Nenhum produto cadastrado.')
+        cadastrar_produto()
+    for produto in produtos:
+        print(f'COD: {produto.codigo}')
+        print(f'PRODUTO: {produto.nome}')
+        print(f'DESCRIÇÃO: {produto.descricao}')
+        print(f'VALOR: R${produto.valor:.2f}')
+        print('-------------------------------------')
+    
+def mostrar_pedidos():
+    if not pedidos:
+        print('Nenhum pedido cadastrado.')
+        cadastrar_pedido()
+    
+    for pedido in pedidos:
+        print(f'Pedido Número: {pedido.codigo}')
+        print(f'Cliente: {pedido.cliente.nome}')
+        
+        total_ped = 0
+        for produto, quantidade in pedido.produtos:
+            total_prod = quantidade * produto.valor
+            print(f'Qtd {quantidade} - {produto.nome} - R$ {produto.valor:.2f}.....R$ {total_prod:.2f}')
+            total_ped += total_prod
+        
+        print(f'Valor Total..................R$ {total_ped:.2f}')
+        print('-------------------------------------')
+
+def salvar_clientes():
+    with open('.\console\ecommerce_app\dados\clientes.txt', 'w') as arquivo:
+        for cliente in clientes:
+            arquivo.write(f'{cliente.codigo},{cliente.nome},{cliente.telefone},{cliente.cpf},{cliente.email}\n')
+
+def salvar_produtos():
+    with open('.\console\ecommerce_app\dados\produtos.txt', 'w') as arquivo:
+        for produto in produtos:
+            arquivo.write(f'{produto.codigo},{produto.nome},{produto.descricao},{produto.valor}\n')
+
+def salvar_pedidos():
+    with open('.\console\ecommerce_app\dados\pedidos.txt', 'w') as arquivo:
+        for pedido in pedidos:
+            produtos_pedidos = ';'.join([f'{prod.codigo}:{qtd}' for prod, qtd in pedido.produtos])
+            arquivo.write(f'{pedido.codigo},{pedido.cliente.codigo},{produtos_pedidos}\n')
+
---
+++ console/ecommerce_app/models.py
@@ -0,0 +1,27 @@
+class Cliente:
+    def __init__(self, codigo, nome, telefone, cpf, email):
+        self.codigo = codigo
+        self.nome = nome
+        self.telefone = telefone
+        self.cpf = cpf
+        self.email = email
+
+class Produto:
+    def __init__(self, codigo, nome, descricao, valor):
+        self.codigo = codigo
+        self.nome = nome
+        self.descricao = descricao
+        self.valor = valor
+
+class Pedido:
+    def __init__(self, codigo, cliente, produtos):
+        self.codigo = codigo
+        self.cliente = cliente
+        self.produtos = produtos  # Lista de tuplas (produto, quantidade)
+        self.valor_total = self.calcular_valor_total()
+
+    def calcular_valor_total(self):
+        total = 0
+        for produto, quantidade in self.produtos:
+            total += produto.valor * quantidade
+        return
